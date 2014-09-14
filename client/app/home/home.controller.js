237f6028f30eb96a537cde6e673d38affc429587
Michael Blasius <mblasiusnyc@gmail.com>
2014-09-14 03:09:29 -0400

    Added Modals

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -4,9 +4,6 @@ angular.module('bitLotteryApp')
   .controller('HomeCtrl', function ($scope, $http, socket) {
 
 //On server load, retrieve all loteries
-
-
-
     $http.get('/api/lotteries').success(function(lotteries) {
       $scope.lotteries = lotteries;
       console.log('lotteries loaded');
--- client/app/modal/modal.controller.js
+++ client/app/modal/modal.controller.js
@@ -1,48 +1,68 @@
 'use strict';
 
 angular.module('bitLotteryApp')
-  .controller('ModalCtrl', function ($scope, $modal, $log) {
-    var ModalCtrl = function ($scope, $modal, $log) {
-      $scope.items = ['item1', 'item2', 'item3'];
+  var ModalCtrl = function ($scope, $modal, $log) {
 
-      $scope.open = function (size) {
+    $scope.items = ['item1', 'item2', 'item3'];
 
-        var modalInstance = $modal.open({
-          templateUrl: 'myModalContent.html',
-          controller: ModalInstanceCtrl,
-          size: size,
-          resolve: {
-            items: function () {
-              return $scope.items;
-            }
+
+    $scope.open = function (lottery, size) {
+      $scope.lottery = lottery;
+      console.log('lottery: ',lottery);
+      console.log('$scope.lottery: ',$scope.lottery);
+      var modalInstance = $modal.open({
+        templateUrl: 'lotteryInfo.html',
+        controller: ModalInstanceCtrl,
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          },
+          lottery: function () {
+            return $scope.lottery;
           }
-        });
+        }
+      });
 
-        modalInstance.result.then(function (selectedItem) {
-          $scope.selected = selectedItem;
-        }, function () {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
-      };
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  };
+
+  // Please note that $modalInstance represents a modal window (instance) dependency.
+  // It is not the same as the $modal service used above.
+
+  var ModalInstanceCtrl = function ($scope, $modalInstance, $modal, lottery) {
+
+    $scope.lottery = lottery;
+    // $scope.selected = {
+    // item: $scope.items[0],
+    // lottery: $scope.lottery
+    // };
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
     };
 
-    // Please note that $modalInstance represents a modal window (instance) dependency.
-    // It is not the same as the $modal service used above.
-
-    var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
-
-      $scope.items = items;
-      $scope.selected = {
-        item: $scope.items[0]
-      };
-
-      $scope.ok = function () {
-        $modalInstance.close($scope.selected.item);
-      };
-
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
+    $scope.openQrModal = function(){
+      $scope.close();
+      var modalInstance = $modal.open({
+        templateUrl: 'lotteryQR.html',
+        controller: ModalInstanceCtrl,
+        size: 'sm',
+        resolve: {
+          lottery: function () {
+            return $scope.lottery;
+          }
+        }
+      });
     };
-  });
+
+    $scope.close = function () {
+      $modalInstance.dismiss('close');
+    };
+  };
 
