60a9d111ecfd84b1a11b991b14d8cb91a8cf3d67
Michael Blasius <mblasiusnyc@gmail.com>
2014-09-23 13:32:53 -0400

    Still working on paying the winner of the lottery

--- server/api/lottery/lottery.controller.js
+++ server/api/lottery/lottery.controller.js
@@ -5,7 +5,12 @@ var async = require('async');
 var request = require('request');
 var secureRandom = require('secure-random');
 var Bitcoin = require('bitcoinjs-lib');
+var convertHex = require('convert-hex');
 var Lottery = require('./lottery.model');
+var bases = require('bases');
+var cs = require('coinstring');
+
+console.log(Bitcoin.convert);
 
 // Get list of lotterys
 exports.index = function(req, res) {
@@ -34,7 +39,6 @@ exports.create = function(req, res) {
   var getBtcAddress = function(done){
     request.post('https://api.blockcypher.com/v1/btc/main/addrs?token=27cdb6a15c19574278edcecb049a4119', function(err, response){
       apiResponseBody = JSON.parse(response.body);
-      // console.log("apiResponseBody:",apiResponseBody)
       console.log("address response from api: ", apiResponseBody)
       newLotteryObj.address = apiResponseBody.address;
       newLotteryObj.publicKey = apiResponseBody.public;
@@ -63,7 +67,7 @@ function createEntrantsWebhook(lottery){
   };
 
   var options = {
-    url: 'https://api.blockcypher.com/v1/btc/test3/hooks',
+    url: 'https://api.blockcypher.com/v1/btc/main/hooks',
     port: 80,
     json: data
   };
@@ -107,132 +111,62 @@ exports.endLottery = function(req, res){
   });
 }
 
-exports.payWinner = function(req,res){
-  var winnerAddress = req.body.winner;
-  var poolAddress = req.body.address;
-  var poolSize = req.body.amountBTC;
-  var poolPublicKey = req.body.publicKey;
-  var poolPrivateKey = req.body.privateKey;
 
-  var bytesToHex = Bitcoin.convert.bytesToHex;
-  var hexToBytes = Bitcoin.convert.hexToBytes;
+exports.payWinner = [
 
-  var rootUrl = "https://api.blockcypher.com/v1/btc/main";
-  // please do not drain our test account, if you need testnet BTC use a faucet
-  // https://tpfaucet.appspot.com/
-  var source = {
-    private : poolPrivateKey,
-    public  : poolPublicKey,
-    address : poolAddress
+// 1. Post our simple transaction information to get back the fully built transaction,
+//    includes fees when required.
+  function newTransaction(req,res,next){
+  //Set variables in function wide scope
+  req.inputSource = {
+    private : req.body.privateKey,
+    public  : req.body.publicKey,
+    address : req.body.address
+  };
+  // console.log(req.inputSource.private);
+  // req.privateKey = req.body.privateKey;
+  console.log('Gets to here')
+  console.log(req.inputSource.private)
+
+  // console.log(bases.toBase58(bases.fromBase16(req.inputSource.private)))
+  req.key = Bitcoin.ECKey.fromWIF(cs.encode(new Buffer(req.inputSource.private, 'hex'), 0x80));
+  console.log(req.key)
+  var newtx = {
+    "inputs": [{"addresses": [req.inputSource.address]}],
+    "outputs": [{"addresses": ['1J7mq2fgXt229LSzFSjWW4ZjD56o1PyzGJ'], "value": -1}]
   }
-  var key   = Bitcoin.ECKey.fromWIF(source.private);
-  var dest  = null;
-
-  // 0. We get a newly generated address
-  // function logAddr(addr) {
-  //   dest = addr;
-  //   log("Generated new address " + dest)
-  // }
-
-  // 1. Post our simple transaction information to get back the fully built transaction,
-  //    includes fees when required.
-  function newTransaction() {
-    var newtx = {
-      "inputs": [{"addresses": [source.address]}],
-      "outputs": [{"addresses": [winnerAddress], "value": -1}]
-    }
-    return request.post(rootUrl+"/txs/new", JSON.stringify(newtx));
-  }
+  request.post({url: "https://api.blockcypher.com/v1/btc/main/txs/new", body: JSON.stringify(newtx)}, function(err, httpResponse, body){
+    req.newtx = JSON.parse(body);
+    next();
+  });
+  },
 
   // 2. Sign the hexadecimal strings returned with the fully built transaction and include
-  //    the source public address.
-  function signAndSend(newtx) {
-    if (checkError(newtx)) return;
+  //    the req.inputSource public address.
+  function signAndSend(req,res,next){
+    // if (checkError(req.unsignedtx)) return;
+    // var newtx = req.newtx;
+    req.newtx.pubkeys  = [];
+    // console.log(req.newtx['tx']);
+    console.log(req.newtx.tosign);
+    req.newtx.signatures  = req.newtx.tosign.map(function(tosign) {
+      req.newtx.pubkeys.push(req.inputSource.public);
+      var tosignBuffer = new Buffer(32);
+      tosignBuffer.write(tosign);
 
-    newtx.pubkeys     = [];
-    newtx.signatures  = newtx.tosign.map(function(tosign) {
-      newtx.pubkeys.push(source.public);
-      return bytesToHex(key.sign(hexToBytes(tosign)));
+      // return bytesToHex(req.key.sign(hexToBytes(tosign)))
+      // return convertHex.bytesToHex(req.key.sign(convertHex.hexToBytes(tosignBuffer)));
+      return Bitcoin.convert.bufferToWordArray((req.key.sign(Bitcoin.convert.wordArrayToBuffer(tosign))));
     });
-    return request.post(rootUrl+"/txs/send", JSON.stringify(newtx));
+
+    request.post({url: "https://api.blockcypher.com/v1/btc/main/txs/send", body: JSON.stringify(req.newtx)}, function(err, httpResonse, body){
+    console.log("body from signAndSend function: ", body);
+      req.finaltx = body;
+    });
   }
+]
 
-  // 3. Open a websocket to wait for confirmation the transaction has been accepted in a block.
-  function waitForConfirmation(finaltx) {
-    if (checkError(finaltx)) return;
-    log("Transaction " + finaltx.tx.hash + " to " + dest.address + " of " +
-          finaltx.tx.outputs[0].value/100000000 + " BTC sent.");
 
-    var ws = new WebSocket("ws://socket.blockcypher.com/v1/btc/test3");
-
-    // We keep pinging on a timer to keep the websocket alive
-    var ping = pinger(ws);
-
-    ws.onmessage = function (event) {
-      if (JSON.parse(event.data).confirmations > 0) {
-        log("Transaction confirmed.");
-        ping.stop();
-        ws.close();
-      }
-    }
-    ws.onopen = function(event) {
-      ws.send(JSON.stringify({filter: "event=new-block-tx&hash="+finaltx.tx.hash}));
-    }
-    log("Waiting for confirmation... (may take > 10 min)")
-  }
-
-  function checkError(msg) {
-    if (msg.errors && msg.errors.length) {
-      log("Errors occured!!/n" + msg.errors.join("/n"));
-      return true;
-    }
-  }
-
-  function pinger(ws) {
-    var timer = setInterval(function() {
-      if (ws.readyState == 1) {
-        ws.send(JSON.stringify({event: "ping"}));
-      }
-    }, 5000);
-    return {stop: function() { clearInterval(timer); }};
-  }
-
-  // function log(msg) {
-  //   request("div.log").append("<div>" + msg + "</div>")
-  // }
-
-function sendTx(sendTxDone){
-  request.post(rootUrl+"/addrs");
-  sendTxDone(null, httpResponse, body);
-}
-
-async.waterfall([sendTx, newTransaction,])
-
-  // Chaining
-
-  //creates a random address
-  // request.post(rootUrl+"/addrs")
-
-//logs out the newly created address
-  // .then(logAddr)
-
-    .then(newTransaction)
-    .then(signAndSend)
-    .then(waitForConfirmation);
-
-  // var txDetails = {
-  //   'inputs': [
-  //     {'addresses': [poolAddress]}
-  //   ],
-  //   'outputs': [
-  //     {'addresses': [winnerAddress], "value": -1}
-  //   ]
-  // }
-  // request.post('https://api.blockcypher.com/v1/btc/main/txs/new', function(response){
-  //   console.log("Response from https://api.blockcypher.com/v1/btc/main/txs/new: ",response);
-  // }).form(txDetails);
-  // res.send(200);
-}
 
 
 
@@ -257,12 +191,6 @@ function decideWinner(lottery){
   // console.table(entrantsObject);
   return winAddress;
 }
-
-
-
-
-
-
 
 
 
--- server/config/seed.js
+++ server/config/seed.js
@@ -9,32 +9,32 @@ var Lottery = require('../api/lottery/lottery.model');
 var User = require('../api/user/user.model');
 
 
-Lottery.find({}).remove(function() {
-  Lottery.create({
-    name: 'First Lottery',
-    info: 'This is the info for the first lottery',
-    address: '1BwP4PxLzA4SnBTyCmCSXbUyANoWiKB52a'.toUpperCase(),
-    publicKey: '044b7ca8cd58ddd86e8fc41bf3d8522922ef0d5a47c6250de9e15f19f236cf9c85554574061c882fd7fab2c75a5e06d6dc39efefa2f4ba9d03f3973fe54b07a2cb',
-    privateKey: '5K5x1xjstRanKxc2bkTrLK6j8SSG2Xz6EAtZ3YQjqJmv7TNyM59',
-    amountBTC: .3,
-    entrants: [{address: '1KfQZXsEZQYFJVLvk5nLYnR1M4iVwX5e6w', amountBTC: .1}, {address:'1MK8sQ82pUpjSTfmCa64tLgPTt1ss6QU4q', amountBTC: .2}],
-    startDate: new Date(),
-    endDate: new Date(2014, 9, 16, 12, 43),
-    winner: ""
-    },
-    {
-    name: 'Second Lottery',
-    info: 'Some 2nd lottery info',
-    address: '17yzW5ZSKUt1qAky2CKBrWDv5R1oHofLqS'.toUpperCase(),
-    publicKey: '04ab037ba9198bd814dca9cf57dfa753a2b4040c78477d158fa6f9b0fb5396c7059e8338875205811df6d89fab9460100de75f4c2824e08e17278bfaa9c485e8f3',
-    privateKey: '5Jpr7UhdrxNM6McxoLyfAX3Y3iTVkDwLfQFzhRbHrRHVUv4fE47',
-    amountBTC: .5,
-    entrants: [{address: '114oEX9xbuP6Afe1Mhw6DQcjyQ9ujNAftx', amountBTC: .1}, {address:'1MK8sQ82pUpjSTfmCa64tLgPTt1ss6QU4q', amountBTC: .2}, {address: '1KfQZXsEZQYFJVLvk5nLYnR1M4iVwX5e6w', amountBTC: .1}],
-    startDate: new Date(),
-    endDate: new Date(2014, 9, 15),
-    winner: ""
-  });
-});
+// Lottery.find({}).remove(function() {
+//   Lottery.create({
+//     name: 'First Lottery',
+//     info: 'This is the info for the first lottery',
+//     address: '1BwP4PxLzA4SnBTyCmCSXbUyANoWiKB52a',
+//     publicKey: '044b7ca8cd58ddd86e8fc41bf3d8522922ef0d5a47c6250de9e15f19f236cf9c85554574061c882fd7fab2c75a5e06d6dc39efefa2f4ba9d03f3973fe54b07a2cb',
+//     privateKey: '5K5x1xjstRanKxc2bkTrLK6j8SSG2Xz6EAtZ3YQjqJmv7TNyM59',
+//     amountBTC: .3,
+//     entrants: [{address: '1KfQZXsEZQYFJVLvk5nLYnR1M4iVwX5e6w', amountBTC: .1}, {address:'1MK8sQ82pUpjSTfmCa64tLgPTt1ss6QU4q', amountBTC: .2}],
+//     startDate: new Date(),
+//     endDate: new Date(2014, 9, 16, 12, 43),
+//     winner: ""
+//     },
+//     {
+//     name: 'Second Lottery',
+//     info: 'Some 2nd lottery info',
+//     address: '17yzW5ZSKUt1qAky2CKBrWDv5R1oHofLqS',
+//     publicKey: '04ab037ba9198bd814dca9cf57dfa753a2b4040c78477d158fa6f9b0fb5396c7059e8338875205811df6d89fab9460100de75f4c2824e08e17278bfaa9c485e8f3',
+//     privateKey: '5Jpr7UhdrxNM6McxoLyfAX3Y3iTVkDwLfQFzhRbHrRHVUv4fE47',
+//     amountBTC: .5,
+//     entrants: [{address: '114oEX9xbuP6Afe1Mhw6DQcjyQ9ujNAftx', amountBTC: .1}, {address:'1MK8sQ82pUpjSTfmCa64tLgPTt1ss6QU4q', amountBTC: .2}, {address: '1KfQZXsEZQYFJVLvk5nLYnR1M4iVwX5e6w', amountBTC: .1}],
+//     startDate: new Date(),
+//     endDate: new Date(2014, 9, 15),
+//     winner: ""
+//   });
+// });
 
 User.find({}).remove(function() {
   User.create({
